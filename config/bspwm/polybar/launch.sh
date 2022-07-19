f43bbff60954764275d80b5d896215d98d70ebb7
Ohm <ohmrc2545@gmail.com>
2022-07-19 13:58:07 -0400

    added theme switch scripts for alacritty and polybar

--- config/bspwm/polybar/launch.sh
+++ config/bspwm/polybar/launch.sh
@@ -1,1 +1,11 @@
-polybar mainbar
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar -c $HOME/.config/bspwm/polybar/config.ini mainbar0 2>&1 | tee -a /tmp/polybar.log & disown
+
+echo "Polybar launched..."
